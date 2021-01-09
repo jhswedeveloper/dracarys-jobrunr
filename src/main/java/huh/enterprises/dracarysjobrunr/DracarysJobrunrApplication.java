966c7ed1e36b3982b68da2c12f0a7441cc960597
Jun Huh <jun.huh@tele2.com>
2021-01-09 01:20:52 +0100

    Initial commit

---
+++ src/main/java/huh/enterprises/dracarysjobrunr/DracarysJobrunrApplication.java
@@ -0,0 +1,13 @@
+package huh.enterprises.dracarysjobrunr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracarysJobrunrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DracarysJobrunrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huh/enterprises/dracarysjobrunr/DracarysJobrunrApplicationTests.java
@@ -0,0 +1,13 @@
+package huh.enterprises.dracarysjobrunr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DracarysJobrunrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
