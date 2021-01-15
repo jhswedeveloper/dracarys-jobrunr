7da04624d767a9ea977cb06f9716d98314de7599
Jun Huh <jun.huh@tele2.com>
2021-01-15 23:24:54 +0100

    java 11

--- src/main/java/io/github/junhuhdev/dracarys/jobrunr/scheduling/package-info.java
+++ src/main/java/io/github/junhuhdev/dracarys/jobrunr/scheduling/package-info.java
@@ -1,16 +1,1 @@
-/**
- * All info on how to enqueue and schedule background jobs.
- * <p>
- * In the JavaDoc the convention regarding generics as follows:
- * <ul>
- *     <li><strong><S></strong>: this represents a service (e.g. available in your IoC container)</li>
- *     <li><strong><T></strong>: this represents an item for which you want to schedule a job</li>
- * </ul>
- *
- * <strong>An example:</strong>
- * <pre>{@code
- *     Stream<User> userStream = userRepository.getAllUsers();
- *     BackgroundJob.<SomeService, User>enqueue(userStream, (x, user) -> x.doWork("do some work for user " + user.getId()));
- * }</pre>
- */
 package io.github.junhuhdev.dracarys.jobrunr.scheduling;
