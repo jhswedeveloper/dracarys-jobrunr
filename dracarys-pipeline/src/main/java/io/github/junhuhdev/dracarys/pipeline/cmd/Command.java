2b8c3cc6f8cde533ac0d0c01b9540024e3db6cb0
Jun Huh <jun.huh@tele2.com>
2021-01-16 06:02:59 +0100

    x

--- dracarys-pipeline/src/main/java/io/github/junhuhdev/dracarys/pipeline/cmd/Command.java
+++ dracarys-pipeline/src/main/java/io/github/junhuhdev/dracarys/pipeline/cmd/Command.java
@@ -2,23 +2,16 @@ package io.github.junhuhdev.dracarys.pipeline.cmd;
 
 import io.github.junhuhdev.dracarys.pipeline.chain.Chain;
 import io.github.junhuhdev.dracarys.pipeline.chain.ChainContext;
-import io.github.junhuhdev.dracarys.pipeline.common.Conditional;
-
-import java.util.UUID;
 
 /**
  * Default interface for all commands to implement.
  */
-public interface Command extends Conditional {
+public interface Command {
 //	/**
 //	 * (1) Proceed to next command "chain.proceed(ctx);"
 //	 * (2) Do not proceed to next command "return ctx"
 //	 */
 //	ChainContext execute(ChainContext ctx, Chain chain) throws Exception;
-
-	default String getId() {
-		return UUID.randomUUID().toString();
-	}
 
 	interface Request extends Command {
 	}
