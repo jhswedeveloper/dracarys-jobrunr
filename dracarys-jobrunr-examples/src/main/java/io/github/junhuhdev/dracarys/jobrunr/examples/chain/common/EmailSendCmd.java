7c0dcef97ad3dae0b318e9706a19ba28c8b89136
Jun Huh <jun.huh@tele2.com>
2021-01-16 02:59:18 +0100

    x

--- dracarys-jobrunr-examples/src/main/java/io/github/junhuhdev/dracarys/jobrunr/examples/chain/common/EmailSendCmd.java
+++ dracarys-jobrunr-examples/src/main/java/io/github/junhuhdev/dracarys/jobrunr/examples/chain/common/EmailSendCmd.java
@@ -1,18 +1,22 @@
-package io.github.junhuhdev.dracarys.jobrunr.examples.chain.user.registration;
+package io.github.junhuhdev.dracarys.jobrunr.examples.chain.common;
 
 import io.github.junhuhdev.dracarys.pipeline.chain.Chain;
 import io.github.junhuhdev.dracarys.pipeline.chain.ChainContext;
 import io.github.junhuhdev.dracarys.pipeline.cmd.Command;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
+@Data
 @Component
-public class UserValidateCmd implements Command {
+public class EmailSendCmd implements Command {
 
-
+	private String from;
+	private String to;
+	private String content;
 
 	@Override
 	public ChainContext execute(ChainContext ctx, Chain chain) throws Exception {
--- dracarys-jobrunr-examples/src/main/java/io/github/junhuhdev/dracarys/jobrunr/examples/chain/user/registration/UserRegistrationChain.java
+++ dracarys-jobrunr-examples/src/main/java/io/github/junhuhdev/dracarys/jobrunr/examples/chain/user/registration/UserRegistrationChain.java
@@ -1,5 +1,7 @@
 package io.github.junhuhdev.dracarys.jobrunr.examples.chain.user.registration;
 
+import io.github.junhuhdev.dracarys.jobrunr.examples.chain.common.EmailSendCmd;
+import io.github.junhuhdev.dracarys.jobrunr.examples.chain.common.SlackNotifyCmd;
 import io.github.junhuhdev.dracarys.pipeline.chain.ChainBase;
 import io.github.junhuhdev.dracarys.pipeline.cmd.Command;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,11 @@ public class UserRegistrationChain extends ChainBase {
 
 	@Override
 	protected List<Class<? extends Command>> getCommands() {
-		return null;
+		return List.of(
+				UserValidateCmd.class,
+				UserCreateCmd.class,
+				EmailSendCmd.class,
+				SlackNotifyCmd.class);
 	}
 
 }
